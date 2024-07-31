ddbb9fcb742c35767bc1c1e67502d4d556cb9c4b
mutpr <honghyen47@gmail.com>
2024-07-31 14:38:44 +0900

    approval modal data transfer ended

--- src/main/java/com/kedu/firmware/controllers/ApprovalController.java
+++ src/main/java/com/kedu/firmware/controllers/ApprovalController.java
@@ -1,33 +1,38 @@
 package com.kedu.firmware.controllers;
 
-import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kedu.firmware.DTO.ApprovalDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
 @RequestMapping("/approval")
 public class ApprovalController {
 
-	
-	@PostMapping
-	public ResponseEntity<Void> signup(@RequestBody List<Object> data) {
-		ApprovalDTO approval = new ApprovalDTO();
-		for(Object object : data) {
-			System.out.println(object);
-		}
-//		for(Object object:data) {
-//			JSONObject parsedData = new JSONObject(object.toString());
-//			System.out.println(parsedData);
-//		}
-		return ResponseEntity.ok().build();
-	}
+    @PostMapping
+    public ResponseEntity<Void> approvalInsertion(@RequestBody Map<String, Object> data) {
+    	ObjectMapper mapper = new ObjectMapper();
+    	String json = (String)data.get("data");
+    	System.out.println(json);
+    	try {
+			Map<String, String> map = mapper.readValue(json, Map.class);
+			System.out.println(map.get("approval_title"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+    	
+        // 추가적인 데이터 처리 로직을 여기에 추가
+        return ResponseEntity.ok().build();
+    }
 }
+
 
