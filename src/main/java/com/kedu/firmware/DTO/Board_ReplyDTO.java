63e58a9f5c4b7cacbe60f0152ca7df044da1054e
JongHo <ehdus232323@naver.com>
2024-08-02 10:24:52 +0900

    board_reply dto 생성

---
+++ src/main/java/com/kedu/firmware/DTO/Board_ReplyDTO.java
@@ -0,0 +1,10 @@
+package com.kedu.firmware.DTO;
+
+import java.sql.Timestamp;
+
+public class Board_ReplyDTO {
+    private int reply_seq;
+    private String reply_uid;
+    private String reply_contents;
+    private Timestamp reply_reg_date;
+}
