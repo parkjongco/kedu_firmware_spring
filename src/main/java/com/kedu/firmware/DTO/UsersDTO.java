3d3c0cdbf5cfcde9dbe3835d317c84b4371f677e
JongHo <ehdus232323@naver.com>
2024-08-20 15:27:00 +0900

    게시판

--- src/main/java/com/kedu/firmware/DTO/UsersDTO.java
+++ src/main/java/com/kedu/firmware/DTO/UsersDTO.java
@@ -1,7 +1,10 @@
 package com.kedu.firmware.DTO;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.sql.Timestamp;
 
+@RestController
 public class UsersDTO {
     private Integer users_seq;
     private String users_code;
